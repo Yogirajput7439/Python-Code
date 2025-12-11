544f7d0182b215663ec2de849f24942848d9b505
yogirajput7439 <yogeshmandawat2324@gmail.com>
2025-12-11 17:24:02 +0530

    this is a problem1.py file

---
+++ problem1.py
@@ -0,0 +1,29 @@
+# username = input("inter you name : ")
+
+# age = int(input("enter your age : "))
+
+# print("hello ", username, "you are ", age, " years old")
+
+# a = int(input("enter a numebr : "))
+# b = int(input("enter a second number : "))
+
+# sum = a + b
+# mul = a * b
+# sub = a - b
+# div = a / b
+# mod = a % b
+
+# print("the sum is : ", sum)
+# print("the multiplication is : ", mul)
+# print("the sub is : ", sub)
+# print("the division is : ", div)
+# print("the modulo is : ", mod)
+# print(type(sum))
+
+b = int(input("enter an integer number : "))
+a = float(input("enter a float numeber : "))
+
+thesum = (a + b)
+
+
+print(thesum)
